c38c1f522a7b88e3973cb1179d13c811987818ab
Amelia Kwiecinska <kwam0015@gmail.com>
2022-03-15 21:59:13 +0100

    routing

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "O ja Ciebie :oo";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('main', 'DefaultController');
+Routing::run($path);
